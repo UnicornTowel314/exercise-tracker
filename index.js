166735b0679a7832dc07a7bd1e3e572d7bd29f40
Sarah Mckee <sarahmckee714@gmail.com>
2025-01-16 16:50:37 -0600

    Added user schema

--- index.js
+++ index.js
@@ -10,14 +10,35 @@ try{
 }catch (err) {
   console.log(err);
 }
+const Schema = mongoose.Schema;
+
+// Mongoose set up
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// User Schema
+const userSchema = new Schema({
+  username: {type: String, required: true}
+});
+let userModel = mongoose.model("user", userSchema);
+
+
 
 app.use(cors())
 app.use(express.static('public'))
+app.use("/", bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
+app.post("/api/users", (req, res) => {
+  let username = req.body.username;
+  let newUser = userModel({ username: username });
+  newUser.save();
+  res.json(newUser);
+});
 
 
 
